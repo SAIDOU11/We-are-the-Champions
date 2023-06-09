a3935dab60aea26c0f07d68860ed1ae2336fc081
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-09 16:21:30 +0200

    clear textarea

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  onValue,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// import {
+//   getDatabase,
+//   ref,
+//   onValue,
+// } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-const appSettings = {
-  databaseURL:
-    "https://realtime-database-41ad1-default-rtdb.europe-west1.firebasedatabase.app",
-};
+// const appSettings = {
+//   databaseURL:
+//     "https://realtime-database-41ad1-default-rtdb.europe-west1.firebasedatabase.app",
+// };
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const endorsementInDB = ref(database, "endorsement");
+// const app = initializeApp(appSettings);
+// const database = getDatabase(app);
+// const endorsementInDB = ref(database, "endorsement");
 
 let plublishBtn = document.getElementById("publish-btn");
 let textArea = document.getElementById("text-area");
 let endorsementList = document.getElementById("endorsement-list");
 
-onValue(endorsementInDB, (snapchot) => {
-  console.log(snapchot.val());
-});
+// onValue(endorsementInDB, (snapchot) => {
+//   console.log(snapchot.val());
+// });
 
 plublishBtn.addEventListener("click", () => {
   let endorsement = textArea.value;
@@ -28,8 +28,17 @@ plublishBtn.addEventListener("click", () => {
   if (endorsement === "") {
     return;
   }
+
+  createEndorsement(endorsement);
+  clearTextArea();
 });
 
-// function clearTextArea() {
-//   endorsement = "";
-// }
+function clearTextArea() {
+  textArea.value = "";
+}
+
+function createEndorsement(value) {
+  let paraEndorsement = document.createElement("p");
+  endorsementList.append(paraEndorsement);
+  paraEndorsement.innerHTML = `${value}`;
+}
